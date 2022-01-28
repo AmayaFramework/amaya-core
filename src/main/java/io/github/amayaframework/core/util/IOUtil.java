18fa5a13dc8360642f7ff706a495f90a91ab4149
Roman Bakaldin <gbakaldin@gmail.com>
2022-01-29 01:59:36 +0300

    Remove redundant dependency

--- src/main/java/io/github/amayaframework/core/util/IOUtil.java
+++ src/main/java/io/github/amayaframework/core/util/IOUtil.java
@@ -1,7 +1,6 @@
 package io.github.amayaframework.core.util;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
