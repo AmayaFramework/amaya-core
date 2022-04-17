f1f33f98f2cfcc2cb3d52bf0d28fcf872aa7656d
RomanQed <gbakaldin@gmail.com>
2022-04-17 14:10:38 +0300

    Add javadocs for Amaya interface

--- src/main/java/io/github/amayaframework/core/Amaya.java
+++ src/main/java/io/github/amayaframework/core/Amaya.java
@@ -1,11 +1,30 @@
 package io.github.amayaframework.core;
 
+/**
+ * A universal interface that encapsulates interaction with the framework server.
+ *
+ * @param <T> server class type
+ */
 public interface Amaya<T> extends AutoCloseable {
+    /**
+     * @return the ip address to which the server was bound
+     */
     String getAddress();
 
+    /**
+     * @return the ip port to which the server was bound
+     */
     int getPort();
 
+    /**
+     * @return the implementation of the internal server
+     */
     T getServer();
 
+    /**
+     * Starts the server
+     *
+     * @throws Throwable if an exception occurs at startup
+     */
     void start() throws Throwable;
 }
