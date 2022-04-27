efb92a08a6c4f0219b65188b1fb790de5291679f
RomanQed <gbakaldin@gmail.com>
2022-04-27 21:10:24 +0300

    Make HttpController public

--- src/main/java/io/github/amayaframework/core/controllers/HttpController.java
+++ src/main/java/io/github/amayaframework/core/controllers/HttpController.java
@@ -9,7 +9,7 @@ import java.util.List;
 /**
  * A class describing a standard http controller.
  */
-class HttpController implements Controller {
+public class HttpController implements Controller {
     private final MethodRouter router;
     private final List<MethodRoute> routes;
     private final String route;
