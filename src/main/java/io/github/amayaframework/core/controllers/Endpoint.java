d5ee860cb8339ea4111bad5dcf79ac6b3dc1efed
RomanQed <gbakaldin@gmail.com>
2022-06-20 12:51:37 +0300

    Make Endpoint documented

--- src/main/java/io/github/amayaframework/core/controllers/Endpoint.java
+++ src/main/java/io/github/amayaframework/core/controllers/Endpoint.java
@@ -2,16 +2,14 @@ package io.github.amayaframework.core.controllers;
 
 import org.atteo.classindex.IndexAnnotated;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * <p>An annotation that is a marker for user controllers by default.</p>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Documented
 @IndexAnnotated
 public @interface Endpoint {
     String value() default "";
