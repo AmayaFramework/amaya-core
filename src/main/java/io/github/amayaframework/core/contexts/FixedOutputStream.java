56cdcc3847a8f697a0ee960cb106ec3671f1a4fd
RomanQed <gbakaldin@gmail.com>
2022-06-29 17:29:24 +0300

    Add checks to FixedOutputStream

--- src/main/java/io/github/amayaframework/core/contexts/FixedOutputStream.java
+++ src/main/java/io/github/amayaframework/core/contexts/FixedOutputStream.java
@@ -5,21 +5,39 @@ import java.io.OutputStream;
 
 public abstract class FixedOutputStream extends OutputStream {
     private final OutputStream stream;
+    protected long length = -1;
 
     public FixedOutputStream(OutputStream stream) {
         this.stream = stream;
+    }
+
+    public long getLength() {
+        return length;
+    }
+
+    private void checkLength(long checked) {
+        if (length <= 0) {
+            throw new IllegalStateException("Unable to write to stream, available length <= 0");
+        }
+        if (checked > length) {
+            throw new IllegalStateException("Unable to write to stream, data too long");
+        }
     }
 
     public abstract void specifyLength(long length) throws IOException;
 
     @Override
     public void write(byte[] b) throws IOException {
+        checkLength(b.length);
         stream.write(b);
+        length -= b.length;
     }
 
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
+        checkLength(len);
         stream.write(b, off, len);
+        length -= len;
     }
 
     @Override
@@ -34,6 +52,8 @@ public abstract class FixedOutputStream extends OutputStream {
 
     @Override
     public void write(int b) throws IOException {
+        checkLength(1);
         stream.write(b);
+        --length;
     }
 }
