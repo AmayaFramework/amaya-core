c7b0999eecd08ac4e518d05fb7375af696280cc6
RomanQed <gbakaldin@gmail.com>
2024-03-20 16:06:12 +0300

    Move service functional to ServiceManager Add interrupted exceptions to do-now functions

--- src/main/java/io/github/amayaframework/core/Application.java
+++ src/main/java/io/github/amayaframework/core/Application.java
@@ -3,13 +3,14 @@ package io.github.amayaframework.core;
 import com.github.romanqed.jconv.Task;
 import io.github.amayaframework.core.configuration.Configuration;
 import io.github.amayaframework.core.context.ApplicationContext;
-import io.github.amayaframework.core.service.Service;
-import io.github.amayaframework.core.service.ServiceCollection;
+import io.github.amayaframework.core.service.ServiceManager;
 import io.github.amayaframework.di.ServiceProvider;
 import io.github.amayaframework.events.EventManager;
 import org.slf4j.Logger;
 
-public interface Application extends Service {
+import java.util.concurrent.Future;
+
+public interface Application extends ServiceManager {
 
     Logger getLogger();
 
@@ -19,11 +20,13 @@ public interface Application extends Service {
 
     Environment getEnvironment();
 
-    ServiceCollection getServiceCollection();
-
     ServiceProvider getServiceProvider();
 
     Task<ApplicationContext> getHandler();
 
     void setHandler(Task<ApplicationContext> handler);
+
+    Future<?> shutdown();
+
+    void shutdownNow() throws InterruptedException;
 }
--- src/main/java/io/github/amayaframework/core/service/ServiceManager.java
+++ src/main/java/io/github/amayaframework/core/service/ServiceManager.java
@@ -2,15 +2,22 @@ package io.github.amayaframework.core.service;
 
 import java.util.concurrent.Future;
 
-public interface Service {
+public interface ServiceManager extends Service {
 
+    ServiceCollection getServiceCollection();
+
+    @Override
     ServiceState getState();
 
+    @Override
     Future<?> start();
 
+    @Override
     Future<?> stop();
 
+    @Override
     boolean startNow() throws InterruptedException;
 
+    @Override
     boolean stopNow() throws InterruptedException;
 }
