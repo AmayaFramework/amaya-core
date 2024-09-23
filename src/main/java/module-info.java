409af1adc41014ea88d75876d70d063add173d8f
Roman Bakaldin <gbakaldin@gmail.com>
2024-09-23 19:32:16 +0300

    Add dependency on options and env modules Improve build manifest

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,7 @@
 open module io.github.amayaframework.core {
+    // Imports
+    requires io.github.amayaframework.options;
+    requires io.github.amayaframework.environment;
+    // Exports
+    // exports io.github.amayaframework.core;
 }
