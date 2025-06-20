8dd513e68fc315c52a97578087a45408213ca072
Roman Bakaldin <gbakaldin@gmail.com>
2025-06-20 16:16:04 +0300

    Add amaya-environment integration to amaya-server

--- server/src/main/java/io/github/amayaframework/server/ServerFactory.java
+++ server/src/main/java/io/github/amayaframework/server/ServerFactory.java
@@ -1,6 +1,7 @@
 package io.github.amayaframework.server;
 
 import io.github.amayaframework.context.Context;
+import io.github.amayaframework.environment.Environment;
 import io.github.amayaframework.options.OptionSet;
 
 /**
@@ -11,12 +12,33 @@ import io.github.amayaframework.options.OptionSet;
 public interface ServerFactory<T extends Context> {
 
     /**
+     * Creates a {@link Server} instance with given options and environment.
+     *
+     * @param set the specified {@link OptionSet} instance, containing initial server options
+     * @param env the specified {@link Environment} instance
+     * @return ready to use, the properly configured {@link Server} instance
+     */
+    default Server<T> create(OptionSet set, Environment env) {
+        return create(set);
+    }
+
+    /**
      * Creates a {@link Server} instance with given options.
      *
      * @param set the specified {@link OptionSet} instance, containing initial server options
      * @return ready to use, the properly configured {@link Server} instance
      */
     Server<T> create(OptionSet set);
+
+    /**
+     * Creates a {@link Server} instance with environment and default options, defined by the factory implementation.
+     *
+     * @param env the specified {@link Environment} instance
+     * @return ready to use, the properly configured {@link Server} instance
+     */
+    default Server<T> create(Environment env) {
+        return create();
+    }
 
     /**
      * Creates a {@link Server} instance with default options, defined by the factory implementation.
