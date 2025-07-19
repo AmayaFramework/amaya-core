322d85fefb9327740456ba53ca1f39a0d29afa5c
Roman Bakaldin <gbakaldin@gmail.com>
2025-07-19 17:04:10 +0300

    Add private ctor for ServiceState

--- service/src/main/java/io/github/amayaframework/service/ServiceState.java
+++ service/src/main/java/io/github/amayaframework/service/ServiceState.java
@@ -1,6 +1,9 @@
 package io.github.amayaframework.service;
 
 public final class ServiceState {
+    private ServiceState() {
+    }
+
     public static final int UNKNOWN = 0;
     public static final int NEW = 1;
     public static final int DISPOSED = 2;
