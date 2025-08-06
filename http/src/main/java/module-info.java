e707e9cce8a44921bfa3ee7fdbe3fa3480f76c89
Roman Bakaldin <gbakaldin@gmail.com>
2025-08-06 14:49:03 +0300

    Rename http module and add javadoc for module-info

--- http/src/main/java/module-info.java
+++ http/src/main/java/module-info.java
@@ -1,4 +1,26 @@
-module io.github.amayaframework.http {
+/**
+ * Provides core HTTP constants and value types used throughout the framework.
+ * <p>
+ * This module defines minimal, reusable representations of common HTTP concepts,
+ * including request methods, protocol versions, status codes, and MIME types.
+ * </p>
+ *
+ * <p>
+ * Included types:
+ * <ul>
+ *     <li>{@link io.github.amayaframework.http.HttpMethod} – standard HTTP request methods</li>
+ *     <li>{@link io.github.amayaframework.http.HttpVersion} – supported HTTP protocol versions</li>
+ *     <li>{@link io.github.amayaframework.http.HttpCode} – predefined HTTP status codes</li>
+ *     <li>{@link io.github.amayaframework.http.MimeType} – commonly used MIME types</li>
+ * </ul>
+ * </p>
+ *
+ * <p>
+ * This module is implementation-agnostic and forms the foundation for
+ * more advanced HTTP handling layers in the framework.
+ * </p>
+ */
+module amayaframework.http {
     // Exports
     exports io.github.amayaframework.http;
 }
